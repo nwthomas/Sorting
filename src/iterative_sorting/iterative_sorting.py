f27e972d830658f773599e5a754346a1215f84aa
Nathan Thomas <nwthomas@me.com>
2019-09-06 20:45:50 -0700

    Tweak insertion_sort() to make tests pass successfully

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,6 +1,6 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    for i in range(0, len(arr) - 1):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+    for i in range(1, len(arr)):
         cur_index = i
         while cur_index > 0 and arr[cur_index] < arr[cur_index - 1]:
             arr[cur_index], arr[cur_index - 1] = arr[cur_index - 1], arr[cur_index]
@@ -9,12 +9,12 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
