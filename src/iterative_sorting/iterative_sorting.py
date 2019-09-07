a32a91de34d0285d539c1c9c95ac31a114172153
Nathan Thomas <nwthomas@me.com>
2019-09-06 20:59:47 -0700

    Build out the bubble_sort() function and pass all tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -10,7 +10,10 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-
+    for i in range(0, len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
     return arr
 
 
