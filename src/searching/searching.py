16d91f658debe904bbe3b3d71ec70ba4005a5354
Nathan Thomas <nwthomas@me.com>
2019-09-08 22:44:52 -0700

    Add stretch non-recursive binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,41 @@
-# STRETCH: implement Linear Search				
+from math import floor
+
+# STRETCH: implement Linear Search
+
+
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
-
-
-# STRETCH: write an iterative implementation of Binary Search 
-def binary_search(arr, target):
-
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
 
   # TO-DO: add missing code
 
-  return -1 # not found
+    return -1   # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
+def binary_search(list, target):
+    if len(list) == 0:
+        return -1  # no items in list
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        guess = floor(low + high / 2)
+        if guess == target:
+            return target
+        elif guess > target:
+            high = guess - 1
+        else:
+            low = guess + 1
+    return -1  # not found
+
+
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    middle = (low+high)//2
+
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+
+
+if __name__ == "__main__":
+    print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 10))
